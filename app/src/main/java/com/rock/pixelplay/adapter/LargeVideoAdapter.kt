1713b5b195524fd89080e6e995c925e6126499d8
anuj-softech <anujkhare375@gmail.com>
2025-03-25 11:02:11 +0530

    added player

--- app/src/main/java/com/rock/pixelplay/adapter/LargeVideoAdapter.kt
+++ app/src/main/java/com/rock/pixelplay/adapter/LargeVideoAdapter.kt
@@ -1,13 +1,18 @@
 package com.rock.pixelplay.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
-import com.rock.pixelplay.databinding.ViewSmallVideoBinding
+import com.rock.pixelplay.databinding.ViewLargeVideoBinding
 import com.rock.pixelplay.helper.VideoUtils
 import com.rock.pixelplay.model.VideoItem
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LargeVideoAdapter(
     private val context: Context, private val videoList: List<VideoItem>
@@ -15,23 +20,41 @@ class LargeVideoAdapter(
 
     val videoUtils = VideoUtils();
 
-    inner class VideoViewHolder(val binding: ViewSmallVideoBinding) :
+    inner class VideoViewHolder(val binding: ViewLargeVideoBinding) :
         RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoViewHolder {
         val binding =
-            ViewSmallVideoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            ViewLargeVideoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return VideoViewHolder(binding)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: VideoViewHolder, position: Int) {
+
         val videoItem = videoList[position]
         holder.binding.title.text = videoItem.title
-        holder.binding.duration.text = videoItem.duration
-        val uri = videoItem.thumbnail.toUri()
-        holder.binding.thumbnail.setImageBitmap(videoUtils.getVideoThumbnail(uri.toString()))
+        holder.binding.duration.text = "${videoItem.lastPlayed} / ${videoItem.duration}"
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val thumbnailBitmap = videoUtils.getVideoThumbnail(videoItem.thumbnail)
+            withContext(Dispatchers.Main) {
+                holder.binding.thumbnail.setImageBitmap(thumbnailBitmap)
+            }
+        }
+
         holder.binding.root.setOnClickListener {
-            videoUtils.playInApp(context,videoItem);
+            videoUtils.playInApp(context, videoItem)
+        }
+        holder.binding.continueBtn.setOnClickListener {
+            videoUtils.playInApp(context, videoItem)
+        }
+
+        holder.binding.progressBar.post {
+            val parentWidth = (holder.binding.progressBar.parent as View).width
+            val newWidth = parentWidth * videoItem.playedPercentage.toInt() / 100
+            holder.binding.progressBar.layoutParams.width = newWidth
+            holder.binding.progressBar.requestLayout()
         }
     }
 
